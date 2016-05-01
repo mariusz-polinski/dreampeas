0095925c204d00d76b2be4913961c28abefe4cb3
klubin <klubin@klubin.nazwa.pl>
2016-05-01 18:18:07 +0200

    file uploads #2

--- wp-content/w3tc-config/master.php
+++ wp-content/w3tc-config/master.php
@@ -421,7 +421,7 @@ return array(
 	'browsercache.other.etag' => false,
 	'browsercache.other.w3tc' => false,
 	'browsercache.other.replace' => false,
-	'browsercache.timestamp' => '1462103584',
+	'browsercache.timestamp' => '1462118985',
 	'browsercache.replace.exceptions' => array(
 	),
 	'mobile.enabled' => false,
